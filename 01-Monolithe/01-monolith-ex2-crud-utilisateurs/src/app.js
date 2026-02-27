e764384fa9b737b977756b09f7e20647d947c218
Constantin <constantin.chtanko@gmail.com>
2026-02-27 09:44:19 +0100

    Ex-2 : CRUD utilisateurs

---
+++ 01-Monolithe/01-monolith-ex2-crud-utilisateurs/src/app.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import userRoutes from './routes/users.js';
+import accountRoutes from './routes/accounts.js';
+
+const app = express();
+
+app.use(express.json());
+
+app.use('/users', userRoutes);
+app.use('/users/:userId/accounts', accountRoutes);
+
+app.get('/', (req, res) => {
+    res.json({ message: "BigBank API v1.0" });
+});
+
+export default app;
---
+++ 01-Monolithe/01-monolith-ex2-crud-utilisateurs/src/services/account.service.js
@@ -0,0 +1,61 @@
+import * as userService from './user.service.js';
+
+let accounts = []
+let nextId = 1;
+
+export function createAccount(userId, data) {
+
+    userService.getUserById(userId);
+
+    const account = {
+        id: nextId++,
+        userId,
+        label: data.label,
+        type: data.type,
+        balance: 0,
+        createdAt: new Date().toISOString()
+    };
+
+    accounts.push(account);
+    return account;
+}
+
+export function getAccountsByUser(userId) {
+
+    userService.getUserById(userId);
+    return accounts.filter(a => a.userId === userId);
+}
+
+export function getAccountById(userId, accountId) {
+
+    userService.getUserById(userId);
+
+    const account = accounts.find(a => a.id === accountId && a.userId === userId);
+    if (!account){
+        throw { status: 404, message: "Compte non trouvé" };
+    }
+    return account;
+}
+
+export function updateAccount(userId, accountId, data){
+
+    userService.getUserById(userId);
+    const index = accounts.findIndex(a => a.id === accountId && a.userId === userId);
+    if (index === -1) {
+        throw { status : 404, message: "Compte non trouvé"};
+    }
+
+    accounts[index] = { ...accounts[index], ...data };
+    return accounts[index];
+}
+
+export function deleteAccount(userId, accountId) {
+
+    userService.getUserById(userId);
+
+    const index = accounts.findIndex(a => a.id === accountId && a.userId === userId);
+    if(index === -1) {
+        throw { status: 404, message: "Compte non trouvé"};
+    }
+    accounts.splice(index, 1);
+}
