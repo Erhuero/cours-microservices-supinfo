6df298b25c19c45a7b568b5ce1fede6645752f88
Constantin <constantin.chtanko@gmail.com>
2026-03-09 14:37:29 +0100

    Delete 02-Microservices/02-microservice-ex2-extraction-service-accounts directory

--- 02-Microservices/02-microservice-ex2-extraction-service-accounts/account-service/index.js
+++
@@ -1,7 +0,0 @@
-import app from './src/app.js';
-
-const PORT = process.env.PORT || 3003;
-
-app.listen(PORT, () => {
-    console.log(`Account Service démarré sur http://localhost:${PORT}`);
-})
--- 02-Microservices/02-microservice-ex2-extraction-service-accounts/bigbank-admin/index.js
+++
@@ -1,7 +0,0 @@
-import app from './src/app.js';
-
-const PORT = process.env.PORT || 3001;
-
-app.listen(PORT, () => {
-    console.log(`BigBank ADMIN API démarrée sur http://localhost:${PORT}`);
-});
