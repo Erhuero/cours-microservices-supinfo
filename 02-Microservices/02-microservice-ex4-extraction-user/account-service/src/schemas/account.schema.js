5aed3cf2d5d13e35d81a84c02a293b762804ffb0
Constantin <constantin.chtanko@gmail.com>
2026-03-11 02:31:47 +0100

    Ex4 extraction user

---
+++ 02-Microservices/02-microservice-ex4-extraction-user/account-service/src/schemas/account.schema.js
@@ -0,0 +1,11 @@
+import { z } from 'zod';
+
+export const createAccountSchema = z.object({
+    userId: z.number().int().positive("userId invalide"),
+    label: z.string().min(1, "Le nom du compte est obligatoire"),
+    type: z.enum(["checking", "savings"], { message: "Type invalide (checking ou savings)"})
+});
+
+export const updateAccountSchema = z.object({
+    label: z.string().min(1).optional()
+});
